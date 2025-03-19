f5df82e0e4123681c824f1129a39d7cb715c5cfb
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-19 15:06:39 +0900

    feat: 자동차 이동 전략 패턴 적용 & 라운드 결과 출력 기능 구현

--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -1,46 +1,41 @@
 package domain;
 
-import java.util.ArrayList;
+import view.OutputView;
+
 import java.util.List;
-import java.util.Random;
 
 public class Racing {
 
-    List<Car> cars = new ArrayList<>();
+    private final Cars cars;
+    private final int roundCount;
+    private final MovingStrategy movingStrategy;
 
-    public List<Car> getCars() {
-        return cars;
+    public Racing(List<String> Names, int roundCount, MovingStrategy movingStrategy) {
+        this.cars = new Cars(Names);
+        this.roundCount = roundCount;
+        this.movingStrategy = movingStrategy;
     }
 
-    public void createCars(int carCount) {
-        for(int i = 0; i < carCount; i++) {
-            this.cars.add(new Car("car" + i));
-        }
-    }
-
-    public void startRacing(int roundNum) {
-        for(int i = 0; i < roundNum; i++) {
+    public void start() {
+        for(int i = 0; i < roundCount; i++) {
             round();
         }
     }
 
     private void round() {
-        for(Car car:cars) {
-            Random random = new Random();
-            int num = random.nextInt(10);
-            car.moveCar(num);
-        }
+        cars.move(movingStrategy);
+        OutputView.printRound(cars);
     }
 
-    public List<Car> findWinner() {
+    public List<Car> getWinners() {
         int maxPosition = findMaxPosition();
-        return cars.stream()
+        return cars.getCars().stream()
                 .filter(c -> c.getPosition() == maxPosition)
                 .toList();
     }
 
     private int findMaxPosition() {
-        return cars.stream()
+        return cars.getCars().stream()
                 .mapToInt(Car::getPosition)
                 .max().orElse(0);
     }
