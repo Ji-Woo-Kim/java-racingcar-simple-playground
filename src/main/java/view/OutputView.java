e85b8cfb824be1658850e907bdd431378e4c2cd0
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-19 14:29:13 +0900

    feat: 출력 클래스 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,18 +1,28 @@
 package view;
 
-import domain.RacingGame;
+import domain.Car;
+import domain.Cars;
 
-public class ResultView {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
 
     public static void printResultMessage() {
         System.out.println("실행 결과");
     }
 
-    public void printRound() {
-
+    public static void printRound(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(String.format("%s : %d", car.getName(), car.getPosition()));
+        }
+        System.out.println(); // 추가적인 공백 출력
     }
 
-    public void printWinners(RacingGame.getWinners()) {
-        System.out.println("가 최종 우승했습니다.");
+    public static void printWinners(final List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(winnerNames + "가 최종 우승했습니다.");
     }
 }
