d07e7c18525cd997a2c920c20c57a12f69259771
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-19 14:29:52 +0900

    feat: Cars 일급 컬렉션 구현

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,4 +1,26 @@
 package domain;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<String> carNames) {
+        this.cars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public void move(MovingStrategy movingStrategy) {
+        for (Car car : cars){
+            car.move(movingStrategy);
+        }
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
 }
