b88d77f51e10b8ea095d8328f18d53821b83fff5
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-19 21:45:35 +0900

    test: 자동차 객체 이동, 정지 테스트 - 함수형 인터페이스 적용

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,29 +1,31 @@
-//package domain;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public class CarTest {
-//
-//    @Test
-//    @DisplayName("0-9 사이의 random 값이 4이상이면 전진한다")
-//    public void moveCar_forward_test() {
-//        Car car = new Car("car0");
-//
-//        car.move(5);
-//
-//        assertEquals(1, car.getPosition());
-//    }
-//
-//    @Test
-//    @DisplayName("0-9 사이의 random 값이 4이하면 멈춘다")
-//    public void moveCar_stop_test() {
-//        Car car = new Car("car0");
-//
-//        car.move(3);
-//
-//        assertEquals(0, car.getPosition());
-//    }
-//}
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("0-9 사이의 random 값이 4이상이면 전진한다.")
+    void forward_test() {
+        Car car = new Car("testCar");
+        MovingStrategy alwaysMoveForward = () -> true;
+
+        car.move(alwaysMoveForward);
+
+        assertEquals(1, car.getPosition());
+    }
+
+    @Test
+    @DisplayName("0-9 사이의 random 값이 4이하면 멈춘다")
+    void stop_test() {
+        Car car = new Car("testCar");
+        MovingStrategy neverMove = () -> false;
+
+        car.move(neverMove);
+
+        assertEquals(0, car.getPosition());
+    }
+}
