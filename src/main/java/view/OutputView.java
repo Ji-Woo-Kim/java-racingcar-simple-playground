fa6a455b3b417565d1c08548c7135ca3ed6c8076
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-22 17:32:20 +0900

    style: 메서드 파라미터, 객체에 final 적용

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,7 +2,6 @@ package view;
 
 import domain.Car;
 import domain.Cars;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,15 +11,15 @@ public class OutputView {
         System.out.println("실행 결과");
     }
 
-    public static void printRound(Cars cars) {
-        for (Car car : cars.getCars()) {
+    public static void printRound(final Cars cars) {
+        for (final Car car : cars.getCars()) {
             System.out.println(car.getName() + ":" + "-".repeat(car.getPosition()));
         }
         System.out.println(); // 추가적인 공백 출력
     }
 
     public static void printWinners(final List<Car> winners) {
-        String winnerNames = winners.stream()
+        final String winnerNames = winners.stream()
                 .map(Car::getName)
                 .collect(Collectors.joining(", "));
         System.out.println(winnerNames + "가 최종 우승했습니다.");
