e88689cc085b0befceee9fb8df8ab334e4367fc7
Jiwoo Kim <ziuoo0424@gmail.com>
2025-03-22 21:36:58 +0900

    "feat: 자동차 이름 검증 로직 적용"

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -4,11 +4,11 @@ import domain.strategy.MovingStrategy;
 
 public class Car {
 
-    private final String name;
+    private final CarName name;
     private int position;
 
     public Car(String name) {
-        this.name = name;
+        this.name = new CarName(name);
         this.position = 0;
     }
 
@@ -19,7 +19,7 @@ public class Car {
     }
 
     public String getName() {
-        return name;
+        return name.name();
     }
 
     public int getPosition() {
